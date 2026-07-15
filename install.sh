c6807598b8eddbdc6d25d0af84e54f55ea6f3a6f
th-martinod <th.martinod@gmail.com>
2026-07-15 16:56:13 -0500

    newnote command update

--- install.sh
+++ install.sh
@@ -7,8 +7,11 @@ DOTFILES_DIR="$HOME/vim-notes/notes-dotfiles"
 [ -d ~/.vim/UltiSnips ] && mv ~/.vim/UltiSnips ~/.vim/UltiSnips.backup
 
 mkdir -p ~/.vim
+mkdir -p ~/.local/bin
 
 ln -s "$DOTFILES_DIR/vimrc" ~/.vimrc
 ln -s "$DOTFILES_DIR/UltiSnips" ~/.vim/UltiSnips
+ln -sf "$DOTFILES_DIR/bin/newnote" ~/.local/bin/newnote
 
 echo "Dotfiles instalados correctamente."
+echo "Asegúrate de que ~/.local/bin esté en tu PATH (agrega 'export PATH=\"\$HOME/.local/bin:\$PATH\"' a tu .bashrc si no lo está)."
